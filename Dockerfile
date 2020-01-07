dccff16295f28ad58bb2ab3b1879213d98c6d240
gilacost <josepgiraltdlacoste@gmail.com>
2020-01-07 15:25:55 +0100

    CI 1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM busybox:latest
+FROM ubuntu:latest
+
 COPY . .
+
+RUN apt-get update
+RUN apt-get install -y busybox apache2
 RUN chmod +x ./entry-point.sh
-CMD ["./entry-point.sh"]
+
+CMD ["/entry-point.sh"]
--- entry-point.sh
+++ entry-point.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cat > index.html <<EOF
-<h1>"Hello World!"</h1> >> index.html
+<h1>"Hello World!"</h1>
 <p>THIS IS THE FIRST VERSION OF THE APP</p>
 EOF
 
-nohup busybox httpd -f -p "${server_port}" &
+nohup busybox httpd -f -p 80
